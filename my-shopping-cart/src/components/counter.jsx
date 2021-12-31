a35e7bc20e335a9f81265ac53a20fee4be968d56
ruchiragoswami <ruchigoswami8266@gmail.com>
2021-12-30 16:27:40 -0800

    Update counter.jsx
    two ways of setting attributes (style)

--- my-shopping-cart/src/components/counter.jsx
+++ my-shopping-cart/src/components/counter.jsx
@@ -2,15 +2,23 @@ import React, { Component } from 'react';
 
 class Counter extends React.Component {
     state = {
-        count: 0
+        count: 0,
+        imgUrl: 'https://picsum.photos/200'
     };
 
+    imgDisplay = {
+        display: "block"
+    }
 
     render() { 
-        return <React.Fragment>         
-            <span> {this.formatCount()} </span>          
-            <button> Increment </button>
-        </React.Fragment>;
+        return ( 
+            <div> 
+                <h1 style= {{fontSize: 35}}> Heading 1 </h1>
+                <img style={this.imgDisplay} src={this.state.imgUrl} />         
+                <span  className='badge badge-primary m-2'> {this.formatCount()} </span>          
+                <button className='btn btn-secondary btn-sm'> Increment </button>
+            </div>
+        );
     }
 
     formatCount() {
