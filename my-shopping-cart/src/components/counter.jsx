6f3799fa895cdf1dde92154d2f6bac289c02c5df
ruchiragoswami <ruchigoswami8266@gmail.com>
2021-12-30 17:45:46 -0800

    Update counter.jsx
    code for rendering classes dynamically

--- my-shopping-cart/src/components/counter.jsx
+++ my-shopping-cart/src/components/counter.jsx
@@ -15,10 +15,22 @@ class Counter extends React.Component {
             <div> 
                 <h1 style= {{fontSize: 35}}> Heading 1 </h1>
                 <img style={this.imgDisplay} src={this.state.imgUrl} />         
-                <span  className='badge badge-primary m-2'> {this.formatCount()} </span>          
+                <span  className={this.getBadgeClasses()}> {this.formatCount()} </span>          
                 <button className='btn btn-secondary btn-sm'> Increment </button>
             </div>
         );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count ===0 ? "warning" : "primary";
+        return classes; 
+        // if (this.state.count === 0) {
+        //     classes += 'badge-warning'
+        // } else {
+        //     classes += 'badge-primary'
+        // }
+        // return classes;
     }
 
     formatCount() {
