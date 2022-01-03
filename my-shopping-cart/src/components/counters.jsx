bbd1dbe951d8398e35756f50d04ae3b0ec1f7a8e
ruchiragoswami <ruchigoswami8266@gmail.com>
2022-01-03 09:49:49 -0800

    checking props
    logged props on console

--- my-shopping-cart/src/components/counters.jsx
+++ my-shopping-cart/src/components/counters.jsx
@@ -5,7 +5,7 @@ import Counter from "./counter";
 class Counters  extends React.Component {
     state = {
         counters : [ 
-            { id :1, value: 0}, 
+            { id :1, value: 5}, 
             { id :2, value: 0},
             { id :3, value: 0},
             { id :4, value: 0},
@@ -18,7 +18,8 @@ class Counters  extends React.Component {
     render() { 
         return (
             <div>
-               {this.state.counters.map( counter => <Counter key={counter.id} />)}
+               {this.state.counters.map( counter => 
+                <Counter key={counter.id} value={counter.value} selected={true} />)}
             </div>
         );
     }
